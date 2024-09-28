468a82e80b14d7828ac3d73dc09f40fb69df78ef
Christopher Serrano <tyson.272002@gmail.com>
2024-09-28 15:30:58 -0500

    Se añadieron funcionalidades de compartir archivos y borrar archivos
    - Se añadió la funcionalidad para que los usuarios registrados en la plataforma puedan enviarse archivos entre si.
    - Quien envia el archivo escoge N cantidad de archivos para enviar y el destinatario (seccion compartir)
    - Quien recibe los archivos puede ir a ver que archivos y quien los ha enviado y decidir si los quiere recibir o no. Si decide recibirlos se copiaran los archivos a su subcarpeta de S3, y si decide no recibirlos no se hara nada. (Seccion recibir)
    - Tambien se añadió la funcionalidad para que los usuarios eliminen sus archivos. Esto ya estaba implementado de cierta forma en el menu de admin, sin embargo, los cambios no se reflejaban en el S3. (ahora si lo hacen)
    - Se añadieron los endpoints, archivos HTMLs, vistas y demas para realizar estos cambios.

--- app_media/models.py
+++ app_media/models.py
@@ -1,14 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+def user_directory_path(instance, filename):
+    return f'archivos_usuarios/{instance.usuario.username}/{filename}'
+
 class Archivo(models.Model):
     nombre = models.CharField(max_length=50)
-    archivo = models.FileField(upload_to='')  
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE) 
+    archivo = models.FileField(upload_to=user_directory_path) 
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
 
     def save(self, *args, **kwargs):
-        self.archivo.name = f'archivos_usuarios/{self.usuario.username}/{self.archivo.name}'
+        if self.archivo:
+            self.archivo.name = f'{self.archivo.name}'
         super().save(*args, **kwargs)
+
+class CompartirArchivo(models.Model):
+    archivo = models.ForeignKey(Archivo, on_delete=models.CASCADE)
+    remitente = models.ForeignKey(User, on_delete=models.CASCADE, related_name='enviado_por')
+    destinatario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recibido_por')
+    aceptado = models.BooleanField(default=False)
+    rechazado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.remitente.username} compartió {self.archivo.nombre} con {self.destinatario.username}"
--- app_media/views.py
+++ app_media/views.py
@@ -1,33 +1,38 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ArchivoForm
-from .models import Archivo
+import boto3
+from .models import Archivo, CompartirArchivo
 from django.contrib.auth.forms import UserCreationForm
 from django.views import View
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 @login_required
 def subir_archivo(request):
     if request.method == 'POST':
         form = ArchivoForm(request.POST, request.FILES)
         if form.is_valid():
-            archivo = form.save(commit=False)  
-            archivo.usuario = request.user 
-            archivo.save() 
+            archivo = form.save(commit=False)
+            archivo.usuario = request.user
+            archivo.save()
             return redirect('archivo_subido_exito')
     else:
         form = ArchivoForm()
     
     return render(request, 'subir_archivo.html', {'form': form})
 
-
 def archivo_subido_exito(request):
     return render(request, 'archivo_subido_exito.html')
 
-
 @login_required  
 def listar_archivos(request):
-    archivos = Archivo.objects.filter(usuario=request.user) 
+    archivos = Archivo.objects.filter(usuario=request.user)
+    for archivo in archivos:
+        archivo.url = f'https://{archivo.archivo.storage.bucket_name}.s3.us-east-2.amazonaws.com/{archivo.archivo.name}'
+    
     return render(request, 'listar_archivos.html', {'archivos': archivos})
+
 
 class RegisterView(View):
     def get(self, request):
@@ -40,3 +45,89 @@ class RegisterView(View):
             form.save()
             return redirect('login')
         return render(request, 'register.html', {'form': form})
+
+@login_required
+def compartir_archivo(request):
+    if request.method == 'POST':
+        archivos_id = request.POST.getlist('archivos')
+        destinatario_id = request.POST.get('destinatario')
+        destinatario = User.objects.get(id=destinatario_id)
+
+        for archivo_id in archivos_id:
+            archivo = Archivo.objects.get(id=archivo_id)
+
+            CompartirArchivo.objects.create(
+                archivo=archivo, 
+                remitente=request.user, 
+                destinatario=destinatario
+            )
+
+        return redirect('listar_archivos')
+
+    archivos = Archivo.objects.filter(usuario=request.user)
+    usuarios = User.objects.exclude(id=request.user.id)
+    return render(request, 'compartir_archivo.html', {'archivos': archivos, 'usuarios': usuarios})
+
+@login_required
+def recibir_archivos(request):
+    archivos_recibidos = CompartirArchivo.objects.filter(destinatario=request.user, aceptado=False, rechazado=False)
+
+    if request.method == 'POST':
+        decision = request.POST.get('decision')
+        archivo_id = request.POST.get('archivo_id')
+        archivo_compartido = get_object_or_404(CompartirArchivo, id=archivo_id)
+
+        if decision == 'aceptar':
+            archivo_compartido.aceptado = True
+            archivo_compartido.save()
+
+            archivo_original = archivo_compartido.archivo
+            
+            nuevo_nombre_archivo = f'archivos_usuarios/{request.user.username}/{archivo_original.nombre}'
+
+            try:
+                s3_client = boto3.client('s3')
+                copy_source = {
+                    'Bucket': 'multimediabucket',
+                    'Key': archivo_original.archivo.name 
+                }
+                
+                s3_client.copy(copy_source, 'multimediabucket', nuevo_nombre_archivo)
+
+                nuevo_archivo = Archivo.objects.create(
+                    nombre=archivo_original.nombre,
+                    archivo=nuevo_nombre_archivo,
+                    usuario=request.user
+                )
+
+                nuevo_archivo.archivo.name = nuevo_nombre_archivo
+                nuevo_archivo.save()
+
+            except Exception as e:
+                print(f"Error al copiar el archivo a S3: {e}")
+
+        elif decision == 'rechazar':
+            archivo_compartido.rechazado = True
+            archivo_compartido.save()
+
+        return redirect('recibir_archivos')
+
+    return render(request, 'recibir_archivos.html', {'archivos_recibidos': archivos_recibidos})
+
+@login_required
+def eliminar_archivo(request, archivo_id):
+    archivo = get_object_or_404(Archivo, id=archivo_id)
+
+    if archivo.usuario == request.user:
+        try:
+            s3_client = boto3.client('s3')
+            file_key = archivo.archivo.name
+            response = s3_client.delete_object(Bucket='multimediabucket', Key=file_key)
+        except s3_client.exceptions.NoSuchKey:
+            print(f"El archivo {file_key} no existe en S3.")
+        except Exception as e:
+            print(f"Error al eliminar el archivo de S3: {e}")
+
+        archivo.delete()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
