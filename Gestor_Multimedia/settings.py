8ec207995ce88d63969055790519713a1e4aba58
Christopher Serrano <tyson.272002@gmail.com>
2024-10-02 09:38:29 -0500

    setup para EC2

--- Gestor_Multimedia/settings.py
+++ Gestor_Multimedia/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.59.154.0', 'localhost']
 
 
 # Application definition
